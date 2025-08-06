51d90a1f69b4d06ed2b45c02a1110e015be6d47b
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-08-06 20:40:08 +0530

    first commit

--- btn-factory/constants/api.js
+++ btn-factory/constants/api.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://btnfactory.onrender.com";
-export const API_URL = "http://10.0.2.2:5000";
+export const API_URL = "https://btnfactory.onrender.com";
+// export const API_URL = "http://10.0.2.2:5000";
 export default API_URL;
 
 // 46B913A4F
